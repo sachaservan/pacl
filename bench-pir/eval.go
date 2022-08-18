2b4aa2c12783dca2c336e0cdc1840ad8519e5941
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2022-08-18 13:06:39 -0400

    Increase number of trials

--- bench-pir/eval.go
+++ bench-pir/eval.go
@@ -17,7 +17,7 @@ func main() {
 
 	n := []int{16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304}
 	byteParams := []int{256, 512, 1024}
-	numTrials := 1
+	numTrials := 10
 
 	i := 0
 	for _, dbSize := range n {
